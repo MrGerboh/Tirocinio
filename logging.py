edd6e45c0c7a8669099ef0ecad62a8ebb0709b5d
MrGerboh <gervasio.1883259@studenti.uniroma1.it>
2022-03-08 12:11:09 +0100

    Creates a file with log informations

--- logging.py
+++ logging.py
@@ -1,2 +1,21 @@
-﻿import os
+﻿import getopt
+import os
 import sys
+
+def main():
+    argv = sys.argv[1:]
+    try:
+        arguments, values = getopt.getopt(argv, 'd:')
+    except getopt.error:
+        print("Can't parse the arguments\nusage: logging.py -d <list of directories>")
+        sys.exit(2)
+    if len(arguments) == 0:
+        print("usage: logging.py -d <list of directories>")
+        sys.exit(2)
+    for arg, val in arguments:
+        #print(f"arg: {arg}, val: {val}")
+        os.chdir(val)
+        os.system('git --no-pager log > log.txt')
+
+if __name__ == '__main__':
+    main()
